feb84ccba8709b217202c09c4ede7ce53db50056
raaviarora01 <90416552+raaviarora01@users.noreply.github.com>
2025-04-21 11:45:23 +0530

    Update 2 HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("Hello World, My name is Raavi");
+		System.out.println("Hello World, My name is Raavi Arora");
 	}
 }
